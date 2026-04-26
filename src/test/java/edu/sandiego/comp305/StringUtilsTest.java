436bb6f80569c46d64c6a369d363c16d06a924a4
Gregory Monzon <gmonzon@sandiego.edu>
2026-04-26 16:55:19 -0700

    deleted unused variables and made assert statements take up multiple lines.

--- src/test/java/edu/sandiego/comp305/StringUtilsTest.java
+++ src/test/java/edu/sandiego/comp305/StringUtilsTest.java
@@ -5,36 +5,36 @@ import static org.junit.jupiter.api.Assertions.*;
 public class StringUtilsTest {
     @Test
     public void testReverseNormalString(){
-        final String testStringUtils = StringUtils.reverseString("hello");
         assertEquals("olleh",
-                StringUtils.reverseString("hello"), "'hello' reversed is 'olleh'.");
+                StringUtils.reverseString("hello"),
+                "'hello' reversed is 'olleh'.");
     }
 
     @Test
     public void testReverseSingleChar(){
-        final String testStringUtils = StringUtils.reverseString("a");
         assertEquals("a",
-                StringUtils.reverseString("a"), "Single characters return themselves.");
+                StringUtils.reverseString("a"),
+                "Single characters return themselves.");
     }
 
     @Test
     public void testReverseEmptyString(){
-        final String testStringUtils = StringUtils.reverseString("");
         assertEquals("",
-                StringUtils.reverseString(""), "Empty strings return themselves.");
+                StringUtils.reverseString(""),
+                "Empty strings return themselves.");
     }
 
     @Test
     public void testReverseWithSpaces(){
-        final String testStringUtils = StringUtils.reverseString("reverse this");
         assertEquals("siht esrever",
-                StringUtils.reverseString("reverse this"), "Reverse strings with spaces");
+                StringUtils.reverseString("reverse this"),
+                "Reverse strings with spaces");
     }
 
     @Test
     public void testReversePalindrome(){
-        final String testStringUtils = StringUtils.reverseString("racecar");
         assertEquals("racecar",
-                StringUtils.reverseString("racecar"), "Palindromes return themselves.");
+                StringUtils.reverseString("racecar"),
+                "Palindromes return themselves.");
     }
 }
